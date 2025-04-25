d1da7d06dee7e0dbd02b0e16604f3a81dd0e7bb1
thichankem <dophuhung.hn@gmail.com>
2025-04-26 01:19:49 +0700

    Create CompactDisc.java

---
+++ AimsProject/srcAims/hust/soict/hedpsi/aims/media/CompactDisc.java
@@ -0,0 +1,50 @@
+package hust.soict.hedspi.aims.media;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompactDisc extends Disc implements Playable {
+    private String artist;
+    private List<Track> tracks = new ArrayList<>();
+
+    public CompactDisc(String title, String category, String director, String artist, int length, float cost) {
+        super(title, category, director, length, cost);
+        this.artist = artist;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void addTrack(Track track) {
+        if (!tracks.contains(track)) {
+            tracks.add(track);
+        }
+    }
+
+    public void removeTrack(Track track) {
+        tracks.remove(track);
+    }
+
+    public int getLength() {
+        int totalLength = 0;
+        for (Track track : tracks) {
+            totalLength += track.getLength();
+        }
+        return totalLength;
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playing CD: " + getTitle());
+        System.out.println("Artist: " + artist);
+        for (Track track : tracks) {
+            track.play();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CD - " + getTitle() + " - " + getCategory() + " - " + artist + " - " + getLength() + " : " + getCost() + "$";
+    }
+}
