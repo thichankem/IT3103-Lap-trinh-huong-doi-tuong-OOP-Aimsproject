c7f88986b4ed6b3522b8cc7b29addfad185b5dae
thichankem <dophuhung.hn@gmail.com>
2025-04-26 01:20:07 +0700

    Create Track.java

---
+++ AimsProject/srcAims/hust/soict/hedpsi/aims/media/Track.java
@@ -0,0 +1,39 @@
+package hust.soict.hedspi.aims.media;
+
+public class Track implements Playable{
+    private String title;
+    private int length;
+
+    public Track(String title, int length) {
+        this.title = title;
+        this.length = length;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playing Track: " + this.title);
+        System.out.println("Track length: " + this.length);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Track) {
+            Track other = (Track) obj;
+            return this.title.equalsIgnoreCase(other.title) && this.length == other.length;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return title.hashCode() + length;
+    }
+}
