fec7e9d5e3609cf7d0af37aaa0de1c03611579f1
thichankem <dophuhung.hn@gmail.com>
2025-04-26 11:33:51 +0700

    Update Book.java

--- AimsProject/src/hust/soict/hedspi/aims/media/Book.java
+++ AimsProject/src/hust/soict/hedspi/aims/media/Book.java
@@ -1,29 +1,20 @@
 package hust.soict.hedspi.aims.media;
 import java.util.ArrayList;
 import java.util.List;
-
-public class Book extends Media{
-    private List<String> authors = new ArrayList<String>();
-
+public class Book extends Media {
+    private List<String> authors = new ArrayList<>();
     public Book(String title) {
         super(title);
     }
-
     public Book(String title, String category, float cost) {
         super(title, category, cost);
     }
-
-
     public List<String> getAuthors() {
-
         return authors;
     }
-
     public void setAuthors(List<String> authors) {
-
         this.authors = authors;
     }
-
     public void addAuthor(String authorName) {
         if (!authors.contains(authorName)) {
             authors.add(authorName);
@@ -32,7 +23,6 @@ public class Book extends Media{
             System.out.println("The author " + authorName + " is already in the store");
         }
     }
-
     public void removeAuthor(String authorName) {
         if (authors.contains(authorName)) {
             authors.remove(authorName);
@@ -40,10 +30,9 @@ public class Book extends Media{
         } else {
             System.out.println("The author " + authorName + " is not in the store");
         }
-
     }
-
+    @Override
     public String toString() {
-        return "Book - Title: " + getTitle() + " - Category: " + getCategory() + " - Authors: " + getAuthors();
+        return "Book - Title: " + getTitle() + " - Category: " + getCategory() + " - Authors: " + authors;
     }
 }
