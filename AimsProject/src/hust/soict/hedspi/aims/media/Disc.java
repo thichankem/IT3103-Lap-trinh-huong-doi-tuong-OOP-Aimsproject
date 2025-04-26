c5aa85d829a28b54f1c25daf7cbb4d9e02ae650c
thichankem <dophuhung.hn@gmail.com>
2025-04-26 10:59:14 +0700

    Create Disc.java

---
+++ AimsProject/src/hust/soict/hedspi/aims/media/Disc.java
@@ -0,0 +1,50 @@
+package hust.soict.hedspi.aims.media;
+
+public abstract class Disc extends Media {
+    private String director;
+    private int length;
+
+    //constructor
+    public Disc(String title) {
+
+        super(title);
+    }
+
+    public Disc(String title, String category, String director, int length, float cost) {
+        super(title, category, cost);
+        this.director = director;
+        this.length = length;
+    }
+
+    public Disc(String title, String category, String director, float cost) {
+        super(title, category, cost);
+        this.director = director;
+    }
+
+    public Disc(String title, String category, float cost) {
+
+        super(title, category, cost);
+    }
+
+
+    public String getDirector() {
+
+        return director;
+    }
+
+    public void setDirector(String director) {
+
+        this.director = director;
+    }
+
+    public void setLength(int length) {
+
+        this.length = length;
+    }
+
+    public int getLength() {
+
+        return length;
+    }
+
+}
