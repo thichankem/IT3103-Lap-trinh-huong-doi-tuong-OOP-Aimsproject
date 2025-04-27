9643660ffa63d914aece73305a88fb60aa7da780
thichankem <dophuhung.hn@gmail.com>
2025-04-27 23:58:02 +0700

    Update DigitalVideoDisc.java

--- AimsProject/src/hust/soict/hedspi/aims/media/DigitalVideoDisc.java
+++ AimsProject/src/hust/soict/hedspi/aims/media/DigitalVideoDisc.java
@@ -1,32 +1,46 @@
 package hust.soict.hedspi.aims.media;
-public class DigitalVideoDisc extends Disc implements Playable {
+import java.util.*;
+public class DigitalVideoDisc extends Disc implements Playable 
+{
     private static int nbDigitalVideoDiscs = 0;
-    public static void setNbDigitalVideoDiscs(int nbDigitalVideoDiscs) {
-        DigitalVideoDisc.nbDigitalVideoDiscs = nbDigitalVideoDiscs;
+    public static int getNbDigitalVideoDiscs() 
+    {
+        return nbDigitalVideoDiscs;
     }
-    public DigitalVideoDisc(String title) {
+    public static void setNbDigitalVideoDiscs(int nbDigitalVideoDiscs) 
+    {
+        if (nbDigitalVideoDiscs>=0) 
+        {
+            DigitalVideoDisc.nbDigitalVideoDiscs = nbDigitalVideoDiscs;
+        } 
+        else 
+            System.out.println("Try again (>=0)");
+    }
+    public DigitalVideoDisc(String title) 
+    {
         super(title);
     }
-    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) 
+    {
         super(title, category, director, length, cost);
     }
-    public DigitalVideoDisc(String category, String title, float cost) {
+    public DigitalVideoDisc(String category, String title, float cost) 
+    {
         super(title, category, cost);
     }
-    public DigitalVideoDisc(String director, String category, String title, float cost) {
+    public DigitalVideoDisc(String director, String category, String title, float cost) 
+    {
         super(title, category, cost);
         setDirector(director);
     }
     @Override
-    public String toString() {
-        return "DVD - Title : " + getTitle()
-             + " - Category : " + getCategory()
-             + " - Director : " + getDirector()
-             + " - Length : " + getLength()
-             + " - Price : " + getCost() + "\n";
+    public String toString() 
+    {
+        return String.format("DVD - Title: %s | Category: %s | Director: %s | Length: %d | Price: %.2f ",getTitle(), getCategory(), getDirector(), getLength(), getCost());
     }
     @Override
-    public void play() {
+    public void play() 
+    {
         System.out.println("Playing DVD: " + getTitle());
         System.out.println("DVD length: " + getLength());
     }
