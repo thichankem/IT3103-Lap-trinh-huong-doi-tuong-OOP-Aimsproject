cdffd456163a8b0bc4c8ad02f67abc6ca0d85c65
thichankem <dophuhung.hn@gmail.com>
2025-04-28 22:38:50 +0700

    Update Store.java

--- AimsProject/src/hust/soict/hedspi/aims/store/Store.java
+++ AimsProject/src/hust/soict/hedspi/aims/store/Store.java
@@ -1,41 +1,54 @@
 package hust.soict.hedspi.aims.store;
 import hust.soict.hedspi.aims.media.DigitalVideoDisc;
 import hust.soict.hedspi.aims.media.Media;
-import java.util.ArrayList;
-import java.util.List;
-public class Store {
+import java.util.*;
+public class Store 
+{
    private ArrayList<Media> itemsInStore = new ArrayList<Media>();
-    public void addMedia(Media media){
-        if (!itemsInStore.contains(media)){
+    public void addMedia(Media media)
+   {
+        if (!itemsInStore.contains(media))
+        {
             itemsInStore.add(media);
-            System.out.println("Added Media: " + media);
+            System.out.println("Added " + media);
         }
     }
-    public void removeMedia(Media media) {
-        if (itemsInStore.remove(media)) {
-            System.out.println("The dvd with title " + media.getTitle() + " has been removed from the cart");
-        } else 
+    public void removeMedia(Media media) 
+   {
+        if (itemsInStore.remove(media)) 
+        {
+            System.out.println("The dvd with title " + media.getTitle() + " has been removed");
+        } 
+        else 
             System.out.println("Can not found the dvd with title " + media.getTitle());
     }
-  public void displayStore(){
+  public void displayStore()
+   {
         System.out.println("**********************CART***********************\n");
-        if (itemsInStore.isEmpty()) {
+        if (itemsInStore.isEmpty()) 
+        {
             System.out.println("There are no dvds in the store");
-        }else
+        }
+        else
             for(int i=0; i<itemsInStore.size(); i++){
                 int j=i+1;
                 System.out.println(j + ". " + itemsInStore.get(i).toString());
         }
   }
-    public List<Media> getItemsInStore() {
+    public List<Media> getItemsInStore() 
+   {
         return itemsInStore;
     }
-    public void setItemsInStore(List<Media> itemsInStore) {
+    public void setItemsInStore(List<Media> itemsInStore)
+   {
         this.itemsInStore = (ArrayList<Media>) itemsInStore;
     }
-    public Media findMediaByTitle(String title) {
-        for (Media media : itemsInStore) {
-            if (media.getTitle().equalsIgnoreCase(title)) {
+    public Media findMediaByTitle(String title)
+   {
+        for (Media media : itemsInStore)
+           {
+            if (media.getTitle().equalsIgnoreCase(title))
+            {
                 return media;
             }
         }
