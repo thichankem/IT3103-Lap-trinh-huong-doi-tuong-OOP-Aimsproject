d852fc288b450ed96dd60b6695e8635bc94603cb
thichankem <dophuhung.hn@gmail.com>
2025-04-28 22:40:14 +0700

    Update StoreTest.java

--- AimsProject/src/hust/soict/hedspi/test/storetest/StoreTest.java
+++ AimsProject/src/hust/soict/hedspi/test/storetest/StoreTest.java
@@ -1,10 +1,10 @@
 package hust.soict.hedspi.aims.storetest;
-
 import hust.soict.hedspi.aims.media.DigitalVideoDisc;
 import hust.soict.hedspi.aims.store.Store;
-
-public class StoreTest {
-    public static void main(String[] args) {
+public class StoreTest
+    {
+    public static void main(String[] args)
+        {
         Store store = new Store();
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
