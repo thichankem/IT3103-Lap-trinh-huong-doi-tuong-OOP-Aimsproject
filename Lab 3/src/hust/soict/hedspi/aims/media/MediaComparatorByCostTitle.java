5d9842bf8e1c9e3ec8b1fced2588857c4e71fe68
thichankem <dophuhung.hn@gmail.com>
2025-05-16 10:01:19 +0700

    Create MediaComparatorByCostTitle.java

---
+++ Lab 3/src/hust/soict/hedspi/aims/media/MediaComparatorByCostTitle.java
@@ -0,0 +1,26 @@
+package hust.soict.hedspi.aims.media;
+import java.util.Comparator;
+
+public class MediaComparatorByCostTitle implements Comparator<Media> {
+    @Override
+    public int compare(Media media1, Media media2) {
+        if (media1.getCost() > media2.getCost()) {
+            return -1;
+        } else if (media1.getCost() < media2.getCost()) {
+            return 1;
+        } else {
+            if (media1.getTitle() != null && media2.getTitle() != null) {
+                return -media1.getTitle().compareTo(media2.getTitle());
+            }
+            if (media1.getTitle() == null && media2.getTitle() != null) {
+                return 1;
+            }
+            if (media1.getTitle() != null && media2.getTitle() == null) {
+                return -1;
+            }
+            return 0;
+
+        }
+    }
+
+}
