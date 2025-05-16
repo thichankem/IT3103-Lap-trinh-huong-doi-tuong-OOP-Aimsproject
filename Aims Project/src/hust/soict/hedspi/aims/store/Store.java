5b3b76d3b147d8369f90ccd77e227777f1f96af3
thichankem <dophuhung.hn@gmail.com>
2025-05-16 09:59:55 +0700

    Create Store.java

---
+++ Aims Project/src/hust/soict/hedspi/aims/store/Store.java
@@ -0,0 +1,62 @@
+package hust.soict.hedspi.aims.store;
+import hust.soict.hedspi.aims.media.Media;
+import java.util.ArrayList;
+import java.util.List;
+public class Store 
+{
+   private ArrayList<Media> itemsInStore = new ArrayList<>();
+    public void addMedia(Media media)
+    {
+        if (!itemsInStore.contains(media))
+        {
+            itemsInStore.add(media);
+            System.out.println("Added Media: " + media);
+        }
+    }
+    public void removeMedia(Media media)
+     {
+        if (itemsInStore.remove(media)) 
+        {
+            System.out.println("The dvd with title " + media.getTitle() + " has been removed from the cart");
+        } 
+        else 
+        {
+            System.out.println("Can not found the dvd with title " + media.getTitle());
+        }
+    }
+  public void displayStore()
+  {
+        System.out.println("**********************CART***********************\n");
+        if (itemsInStore.isEmpty()) 
+        {
+            System.out.println("There are no dvds in the store");
+        }
+        else
+        {
+            for(int i=0; i<itemsInStore.size(); i++)
+            {
+                int j=i+1;
+                System.out.println(j + ". " + itemsInStore.get(i).toString());
+            }
+        }
+  }
+    public List<Media> getItemsInStore() 
+    {
+        return itemsInStore;
+    }
+    public void setItemsInStore(List<Media> itemsInStore) 
+    {
+        this.itemsInStore = (ArrayList<Media>) itemsInStore;
+    }
+    public Media findMediaByTitle(String title) 
+    {
+        for (Media media : itemsInStore) 
+        {
+            if (media.getTitle().equalsIgnoreCase(title)) 
+            {
+                return media;
+            }
+        }
+        return null;
+    }
+}
