5f52e9d740a8445dbf9f6254024a57a118830ebc
thichankem <dophuhung.hn@gmail.com>
2025-05-30 08:18:08 +0700

    Create LimitExceededException.java

---
+++ Aims Project/src/hust/soict/hedspi/aims/exception/LimitExceededException.java
@@ -0,0 +1,8 @@
+package hust.soict.hedspi.aims.exception;
+public class LimitExceededException extends Exception
+{
+	public LimitExceededException(String string) 
+    {
+        super(string);
+    }
+}
